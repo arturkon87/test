84075489be801f101f63287e90ac72958ab37182
Artur <artur@aventia.no>
2024-05-27 14:40:53 +0200

    fafa

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,14 +8,18 @@ import { Todo } from "../shared/interfaces/todo.interface";
   styleUrl: "./todo-list.component.css",
 })
 export class TodoListComponent {
+  clearErrorMessage() {
+    this.errorMessage = "";
+  }
   changeTodoStatus(todo: Todo) {
     todo.isComplete = !todo.isComplete;
     console.log(this.todos);
   }
   todos: Todo[] = [];
+  errorMessage = "";
   addTodo(todo: string): void {
     if (todo.length <= 3) {
-      alert("The task must be at least 4 characters long");
+      this.errorMessage = "The task must be at least 4 characters long";
       return;
     }
     this.todos.push({ name: todo, isComplete: false });
